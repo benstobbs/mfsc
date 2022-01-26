8be41e02e246584ecbab5ae9696362c9c2dfb977
Ben Stobbs <ben@stobbs.com>
2022-01-26 11:27:00 +0000

    added linux and darwin for open-cad-suite install

--- src/scripts/install.ts
+++ src/scripts/install.ts
@@ -2,16 +2,34 @@ import { rename, renameSync } from "fs";
 import { execFile } from "child_process";
 import download = require('download');
 
+const ossVersion = {
+    y: "2022",
+    m: "01",
+    d: "26"
+};
+
 async function main (){
     const isWindows = process.platform === "win32";
-    const exeExtension = isWindows ? ".exe" : "";
+    const archiveExtension = isWindows ? ".exe" : ".tgz";
+    var ossUrlOS;
+
+    switch(process.platform){
+        case "win32": {ossUrlOS = "windows"; break;}
+        case "darwin": {ossUrlOS = "darwin"; break;}
+        case "linux": {ossUrlOS = "linux"; break;}
+        default: {
+            throw new Error('Unsupported operating system.');
+        }
+    }
+
+    const ossDownloadUrl = `https://github.com/YosysHQ/oss-cad-suite-build/releases/download/${ossVersion.y}-${ossVersion.m}-${ossVersion.d}/oss-cad-suite-${ossUrlOS}-x64-${ossVersion.y}${ossVersion.m}${ossVersion.d}${archiveExtension}`;
 
     await download(
-        "https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2022-01-26/oss-cad-suite-windows-x64-20220126.exe",
+        ossDownloadUrl,
         "./tools/",
         {
             extract: !isWindows,
-            filename: "tools" + exeExtension
+            filename: "tools" + archiveExtension
         }
     );
 
