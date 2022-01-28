b578ce6c85cb13c4efb3b213e48a24efb3cd5db3
Ben Stobbs <ben@stobbs.com>
2022-01-28 20:35:22 +0000

    added more tabby environment variables from environment scripts

--- src/helpers.ts
+++ src/helpers.ts
@@ -16,18 +16,20 @@ export function execToolSync(tool: string, args: string[] = [], cwd: string = ".
     const libPath = path.join(cadPath, "lib");
     const py3binPath = path.join(cadPath, "py3bin");
 
-    const pathSeparator = process.platform === "win32" ? ";" : ":";
+    const isWindows = process.platform === "win32";
+
+    const pathSeparator = isWindows ? ";" : ":";
 
     const pathEnv = `${binPath}${pathSeparator}${libPath}${pathSeparator}${py3binPath}${pathSeparator}${process.env.PATH}`;
 
-    if (process.platform !== "win32"){
-        if (tool === "python3"){
-            tool = "tabbypy3";
-        }
-        else if (tool === "pip" || tool === "pip3"){
-            tool = "tabbypip";
-        }
-    }
+    // if (!isWindows){
+    //     if (tool === "python3"){
+    //         tool = "tabbypy3";
+    //     }
+    //     else if (tool === "pip" || tool === "pip3"){
+    //         tool = "tabbypip";
+    //     }
+    // }
 
     console.log(`Executing tool ${tool} with args ${args} and PATH = ${pathEnv}`);
 
@@ -36,7 +38,17 @@ export function execToolSync(tool: string, args: string[] = [], cwd: string = ".
         args,
         {
             env: {
-                "PATH": pathEnv
+                "PATH": pathEnv,
+                "PYTHONEXECUTABLE": path.join(binPath, "python3"),
+                "PYTHONHOME": cadPath,
+                "PYTHONNOUSERSITE": "1",
+                "SSL_CERT_FILE": path.join(cadPath, "etc", "cacert.pem"),
+                "QT_PLUGIN_PATH": path.join(libPath, "qt5", "plugins"),
+                "QT_LOGGING_RULES": "*=false",
+                "GTK_EXE_PREFIX": cadPath,
+                "GTK_DATA_PREFIX": cadPath,
+                "GDK_PIXBUF_MODULEDIR": path.join(libPath, "gdk-pixbuf-2.0", "2.10.0", "loaders"),
+                "GDK_PIXBUF_MODULE_FILE": path.join(libPath, "gdk-pixbuf-2.0", "2.10.0", "loaders.cache")
             },
             cwd: cwd
         }    
