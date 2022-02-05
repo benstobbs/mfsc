5b804291087272c1eca1ebca849d62310f924f60
Ben Stobbs <ben@stobbs.com>
2022-02-05 15:47:51 +0000

    add test to build orangecrab soc

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,26 @@
 import * as assert from 'assert';
+import path = require('path');
+import { existsSync, mkdirSync } from 'fs';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Run Build SoC Command', async () => {
+		const testProjectFolder = path.resolve("extension_test_project");
+
+		if (!existsSync(testProjectFolder)){
+			mkdirSync(testProjectFolder);
+		}
+		
+		let uri = vscode.Uri.file(testProjectFolder);
+		await vscode.commands.executeCommand('vscode.openFolder', uri);
+		
+		await vscode.commands.executeCommand("mfsc.compileSoC").then(() => {
+			assert.strictEqual(existsSync(path.join(testProjectFolder, "build", "gateware", "gsd_orangecrab.bit")), true);
+		});
 	});
 });
