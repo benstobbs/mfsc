cbd33e6a4b72eea1afef5a577b8f0b7652b8979c
Ben Stobbs <ben@stobbs.com>
2022-02-05 15:47:41 +0000

    change execSync to execFileSync

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { execFileSync, execSync } from 'child_process';
 import { existsSync, mkdirSync } from 'fs';
 import path = require('path');
 import * as vscode from 'vscode';
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 			const workspacePath = workspaceFolders[0].uri.path;
 			var buildFolder = path.join(workspacePath, "build");
 
-			// /c:/... --> c:/...
+			// \c:\... --> c:\...
 			if (process.platform === "win32" && buildFolder[0] === "\\"){
 				buildFolder = buildFolder.slice(1);
 			}
@@ -23,8 +23,19 @@ export function activate(context: vscode.ExtensionContext) {
 				mkdirSync(buildFolder);
 			}
 
-			const command = `docker run --rm -v ${buildFolder}:/project benstobbs/litex-runner python3 /litex/litex-boards/litex_boards/targets/gsd_orangecrab.py --build --output-dir /project/`;
-			execSync(command);
+			const args = [
+				"run",
+				"--rm",
+				"-v",
+				`${buildFolder}:/project`,
+				"benstobbs/litex-runner",
+				"python3",
+				"/litex/litex-boards/litex_boards/targets/gsd_orangecrab.py",
+				"--build",
+				"--output-dir",
+				"/project/"
+			];
+			execFileSync("docker", args, {maxBuffer: 100 * 1024 * 1024});
 			
 			vscode.window.showInformationMessage("Done!");
 		}
