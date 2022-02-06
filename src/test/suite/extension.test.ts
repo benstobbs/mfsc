fc85eb03c012913ce1727ea374d747768bed3268
Ben Stobbs <ben@stobbs.com>
2022-02-06 21:43:44 +0000

    add create project command test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -6,18 +6,36 @@ import { existsSync, mkdirSync } from 'fs';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 
+async function openTestFolder() {
+	const testProjectFolder = path.resolve("extension_test_project");
+
+	if (!existsSync(testProjectFolder)){
+		mkdirSync(testProjectFolder);
+	}
+	
+	let uri = vscode.Uri.file(testProjectFolder);
+	await vscode.commands.executeCommand('vscode.openFolder', uri);
+
+	return testProjectFolder;
+}
+
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Run Build SoC Command', async () => {
-		const testProjectFolder = path.resolve("extension_test_project");
+	test('Run Create Project Command', async () => {
+		const testProjectFolder = await openTestFolder();
 
-		if (!existsSync(testProjectFolder)){
-			mkdirSync(testProjectFolder);
-		}
-		
-		let uri = vscode.Uri.file(testProjectFolder);
-		await vscode.commands.executeCommand('vscode.openFolder', uri);
+		await vscode.commands.executeCommand("mfsc.createProject").then(() => {
+			const files = ["main.c", "isr.c", "linker.ld", "Makefile"];
+
+			files.forEach((file) => {
+				assert.strictEqual(existsSync(path.join(testProjectFolder, file)), true);
+			});
+		});
+	});
+
+	test('Run Build SoC Command', async () => {
+		const testProjectFolder = await openTestFolder();
 		
 		await vscode.commands.executeCommand("mfsc.compileSoC").then(() => {
 			assert.strictEqual(existsSync(path.join(testProjectFolder, "build", "gateware", "gsd_orangecrab.bit")), true);
