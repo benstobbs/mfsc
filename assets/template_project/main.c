d8ffd38b53e9986d2783a34a8d2c88534ffbd688
Ben Stobbs <ben@stobbs.com>
2022-02-08 16:30:12 +0000

    update main.c to init interrupts and uart

--- assets/template_project/main.c
+++ assets/template_project/main.c
@@ -2,8 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <generated/csr.h>
+#include <irq.h>
+#include <libbase/uart.h>
 
 int main(void) {
+#ifdef CONFIG_CPU_HAS_INTERRUPT
+	irq_setmask(0);
+	irq_setie(1);
+#endif
+	uart_init();
+
 	printf("Hello World!");
 	return 0;
 }
